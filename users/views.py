4c5b80c6b921c7e2f902e1d740a7f4b12f5bd3e6
AtharvaUtekar <atharva.v.utekar@gmail.com>
2023-07-21 12:44:27 +0530

    Register User View created

--- users/views.py
+++ users/views.py
@@ -5,16 +5,18 @@ from rest_framework.views import APIView
 # from .serializers import CustomUserSerializer
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.permissions import AllowAny
+from .serializers import RegisterUserSerializer
 
 
 class CustomUserCreate(APIView):
     permission_classes = [AllowAny]
 
-    def post(self, request, format='json'):
+    def post(self, request):
         # serializer = CustomUserSerializer(data=request.data)
-        if serializer.is_valid():
-            user = serializer.save()
-            if user:
-                json = serializer.data
-                return Response(json, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        reg_serializer = RegisterUserSerializer(data=request.data)
+        if reg_serializer.is_valid():
+            newuser = reg_serializer.save()
+            if newuser:
+                # json = serializer.data
+                return Response(status=status.HTTP_201_CREATED)
+        return Response(reg_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
